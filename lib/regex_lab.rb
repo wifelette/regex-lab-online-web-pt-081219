378fcb7f2e1876bbeb4decc8177fc59f31e2571d
Leah Silber <leahsilber@gmail.com>
2019-08-31 06:37:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+	!!word.match(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+	text.scan(/\b[unUN]\S*[ingING]\b/)
 end
 
 def words_five_letters_long(text)
-
+	text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+	!!text.match(/\A[A-Z].*\p{Punct}\z/)
 end
 
 def valid_phone_number?(phone)
-
+	!!phone.match?(/\A(\d{3})[\s-]?(\d{3})[\s-]?(\d{4})\z|\A\d{10}\z|\A\(?(\d{3})\)?(\d{3})-?(\d{4})\z/)
 end
