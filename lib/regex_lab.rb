fa4e2ecbb53e30719ce1b8d0c633b0441b3e3c6c
Leah Silber <leahsilber@gmail.com>
2019-08-31 06:15:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+	!!text.match(/\A[A-Z].*\p{Punct}\z/)
 end
 
 def valid_phone_number?(phone)
